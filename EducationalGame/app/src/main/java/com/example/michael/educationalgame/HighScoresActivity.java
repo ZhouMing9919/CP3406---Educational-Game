661c0f22a927d901cf617376bfae6c582679b32e
michaelkoppen <mkoppen98@hotmail.com>
2018-05-07 15:33:53 +1000

    something

--- EducationalGame/app/src/main/java/com/example/michael/educationalgame/HighScoresActivity.java
+++ EducationalGame/app/src/main/java/com/example/michael/educationalgame/HighScoresActivity.java
@@ -1,7 +1,11 @@
 package com.example.michael.educationalgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
